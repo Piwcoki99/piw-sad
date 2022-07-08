214e8c453a4e55be300910810cc29b6ef52a415f
Nano99 <108853828+Piwcoki99@users.noreply.github.com>
2022-07-08 12:13:31 +0700

    Update piwbot2.js

--- plugins/piwbot2.js
+++ plugins/piwbot2.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let helloarie = fs.readFileSync('./media/Bot23.mp3') 
+let helloarie = fs.readFileSync('./media/towol1.opus') 
 conn.sendFile(m.chat, helloarie, '', '', m, true)
 //conn.sendMessage(m.chat, helloarie, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloarie }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(desah|#desah)$/i
+handler.customPrefix = /^(dessah#1desah)$/i
 handler.command = new RegExp
 
 handler.limit = true
